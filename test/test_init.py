8fd382871dd876b89fc58b8363641f4891289439
Mathieu Boudreau <emb6150@gmail.com>
2020-06-12 18:46:19 -0230

    Fix bug in test

--- test/test_init.py
+++ test/test_init.py
@@ -5,14 +5,7 @@ import pytest
 import pyqmrlab
 
 class TestCore(object):
-    def setup(self):
-
-    @classmethod
-    def teardown_class(cls):
-
 
     # --------------module tests-------------- #
-    @pytest.mark.unit
     def test_qmrlab_module(self):
-
         assert pyqmrlab.__version__
