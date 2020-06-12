fad94380905b96bba27dbba92a881a95a84a7adc
Mathieu Boudreau <emb6150@gmail.com>
2020-06-12 18:39:40 -0230

    Add module init test

---
+++ test/test_init.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+import pytest
+
+import pyqmrlab
+
+class TestCore(object):
+    def setup(self):
+
+    @classmethod
+    def teardown_class(cls):
+
+
+    # --------------module tests-------------- #
+    @pytest.mark.unit
+    def test_qmrlab_module(self):
+
+        assert pyqmrlab.__version__
