27de1726e75dad9f20bf15896e3dd5fba6f2d1e2
Mathieu Boudreau <emb6150@gmail.com>
2020-06-13 01:14:35 -0230

    Add MTR module

---
+++ pyqmrlab/mt/mtr.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+# Scientific modules imports
+import numpy as np
+import scipy.io as sio
+import pyqmrlab.utils as utils
+from pathlib import Path
+import nibabel as nib
+np.seterr(divide='ignore', invalid='ignore')
+
+class mtr:
+    def __init__(self):
+        self.data_url = 'https://osf.io/erm2s/download?version=1'
+        self.inputs = [
+            'MTon',
+            'MToff',
+            'Mask'
+            ]
+        
+        self.outputs = [
+            'MTR'
+        ]
+
+    def download(self, folder=None):
+        if folder==None:
+            utils.download_data(self.data_url)
+        else:
+            utils.download_data(self.data_url, folder)
+    
+    def load(self, MTon, MToff, Mask = None):
+        args = locals()
+        for key, value in args.items():
+            if key != 'self' and value != None:
+                filepath = Path(value)
+                if '.mat' in filepath.suffixes:
+                    matDict = sio.loadmat(filepath)
+                    setattr(self, key, matDict[key])
+
+    def save(self, filename = None):
+
+        if filename == None:
+            filename = 'MTR.nii.gz'
+
+        img = nib.Nifti1Image(self.MTR, affine = None,  header = None)
+        nib.save(img, filename)
+
+    def fit(self):
+        self.MTR = (self.MToff - self.MTon) / self.MToff * 100
+
+        if hasattr(self, 'Mask'):
+            self.Mask[np.isnan(self.Mask)] = 0
+
+            self.Mask = self.Mask.astype(bool)
+            self.MTR = self.MTR * self.Mask
+        
+        self.MTR[np.isnan(self.MTR)] = 0
---
+++ test/mtr/test_mtr.py
@@ -0,0 +1,98 @@
+# coding: utf-8
+
+import pytest
+from pathlib import Path
+from pyqmrlab.mt import mtr
+import httplib2
+import shutil
+import numpy as np
+
+class TestCore(object):
+    def setup(self):
+        self.tmpPath = Path('tmp/')
+
+    @classmethod
+    def teardown_class(cls):
+        tmpPath = Path('tmp/')
+        shutil.rmtree(tmpPath)
+
+    # --------------attribute tests-------------- #
+    def test_data_url_link_exists(self):
+        
+        mtr_obj = mtr()
+
+        h = httplib2.Http()
+        
+        try:
+            resp = h.request(mtr_obj.data_url, 'HEAD')
+            assert int(resp[0]['status']) < 400
+        except Exception:
+            pytest.fail('Website not found.')
+
+    # --------------download tests-------------- #
+    def test_download(self):
+        mtr_obj = mtr()
+        mtr_obj.download(self.tmpPath)
+
+        expected_folder = self.tmpPath / "mt_ratio"
+        expected_files = [
+            self.tmpPath / 'mt_ratio/MToff.mat', 
+            self.tmpPath / 'mt_ratio/Mask.mat', 
+            self.tmpPath / 'mt_ratio/MTon.mat'
+            ]
+        assert expected_folder.is_dir()
+        for file in expected_files:
+            assert file.is_file()
+
+    # --------------load tests-------------- #
+    def test_load(self):
+        mtr_obj = mtr()
+
+        MTon = self.tmpPath / 'mt_ratio/MTon.mat'
+        MToff = self.tmpPath / 'mt_ratio/MToff.mat'
+        Mask = self.tmpPath / 'mt_ratio/Mask.mat'
+        
+        mtr_obj.load(MTon, MToff, Mask)
+        
+        assert isinstance(mtr_obj.MTon, np.ndarray)
+        assert isinstance(mtr_obj.MToff, np.ndarray)
+        assert isinstance(mtr_obj.MToff, np.ndarray)
+
+        expected_shape = (128, 135, 75)
+        assert mtr_obj.MTon.shape == expected_shape
+        assert mtr_obj.MToff.shape == expected_shape
+        assert mtr_obj.MToff.shape == expected_shape
+
+    # --------------fit tests-------------- #
+    def test_fit(self):
+        mtr_obj = mtr()
+
+        MTon = self.tmpPath / 'mt_ratio/MTon.mat'
+        MToff = self.tmpPath / 'mt_ratio/MToff.mat'
+        Mask = self.tmpPath / 'mt_ratio/Mask.mat'
+        
+        mtr_obj.load(MTon, MToff, Mask)
+        
+        mtr_obj.fit()
+
+        expected_mean_value = 43.183624
+        actual_mean_value = np.mean(mtr_obj.MTR[mtr_obj.MTR!=0])
+
+        assert actual_mean_value == pytest.approx(expected_mean_value)
+
+    # --------------save tests-------------- #
+    def test_save(self):
+        mtr_obj = mtr()
+
+        MTon = self.tmpPath / 'mt_ratio/MTon.mat'
+        MToff = self.tmpPath / 'mt_ratio/MToff.mat'
+        Mask = self.tmpPath / 'mt_ratio/Mask.mat'
+        
+        mtr_obj.load(MTon, MToff, Mask)
+        
+        mtr_obj.fit()
+
+        outputFile = self.tmpPath / 'MTR.nii.gz'
+        mtr_obj.save(outputFile)
+
+        assert outputFile.is_file()
