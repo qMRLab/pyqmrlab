0e23af8985bfa7b2e96dc1c90009548160c5f888
Mathieu Boudreau <emb6150@gmail.com>
2020-07-10 23:44:49 -0300

    Add documentation to classes and refactor names

--- pyqmrlab/b1/double_angle.py
+++ pyqmrlab/b1/double_angle.py
@@ -1,26 +1,75 @@
 # coding: utf-8
+"""Calculate magnetization transfer ratio (MTR)
 
-# Scientific modules imports
-import numpy as np
-import pyqmrlab.utils as utils
-from pyqmrlab.abstract import Abstract
+This module is for calculating MTR from magnetization transfer (MT)-weighted
+MRI data.
+
+  Typical usage example:
+
+  from pathlib import Path
+  from pyqmrlab.b1 import DoubleAngle
+
+  b1_obj = DoubleAngle()
+  
+  # Download sample dataset
+  b1_obj.download(folder='data/')
+
+  # Load data
+  data_folder = Path('data/b1_dam_multi-slice/')
+  b1_obj.load(
+      img1 = data_folder / 'epseg_60deg.nii.gz',
+      img2 = data_folder / 'epseg_120deg.nii.gz',
+      )
+
+  # Fit data
+  b1_obj.fit()
+
+  # Save to NIFTI
+  b1_obj.save(filename = data_folder / 'B1.nii')
+"""
+
+from pyqmrlab.abstract import *
 
 np.seterr(divide="ignore", invalid="ignore")
 
 
-class double_angle(Abstract):
+class DoubleAngle(Abstract):
+    """Double angle B1 mapping data processing class
+
+    Fits double angle B1 mapping data and saves to NIfTI. Demo dataset
+    available for download.
+
+    Attributes:
+        data_url: Link to demo dataset.
+        params: Dictionnary of pulse sequence parameters for each 
+                measurements. Measurement keys: flip_angle (degrees).
+    """
+
     data_url = "https://osf.io/kytxw/download?version=1"
+    params = None
 
     def __init__(self, params=None):
+        """Initializes a DoubleAngle object.
+
+        Assigns double angle B1 mapping pulse sequence parameters to the
+        `params` attribute, or if none are given assigns the parameters to be
+        used with the demo data.
+
+        Args:
+        params: Dictionnary of pulse sequence parameters for each 
+                measurements. Measurement keys: flip_angle (degrees).
+
+        Returns:
+            DoubleAngle class object with parameters initialized.
+        """
         if params == None:
-            self.params = {"FA": 60}
+            self.params = {"flip_angle": 60}
 
     def load(self, img1, img2, Mask=None):
         args = locals()
         super().load(args)
 
     def save(self, filename=None):
-
         if filename == None:
             filename = "B1.nii.gz"
 
@@ -29,8 +78,8 @@ class double_angle(Abstract):
     def fit(self):
         img1 = self.img1
         img2 = self.img2
-        FA = np.deg2rad(self.params["FA"])
+        flip_angle = np.deg2rad(self.params["flip_angle"])
 
-        self.B1 = np.abs(np.arccos(img2 / (2 * img1)) / (FA))
+        self.B1 = np.abs(np.arccos(img2 / (2 * img1)) / (flip_angle))
 
         self._apply_mask(B1=self.B1)
--- pyqmrlab/mt/mtsat.py
+++ pyqmrlab/mt/mtsat.py
@@ -1,27 +1,78 @@
 # coding: utf-8
+"""Calculate magnetization transfer saturation (MTsat)
 
-# Scientific modules imports
-import numpy as np
-import scipy.io as sio
-import pyqmrlab.utils as utils
-from pathlib import Path
-import nibabel as nib
-from pyqmrlab.abstract import Abstract
+This module is for calculating MTsat from magnetization transfer (MT)-weighted
+MRI data.
+
+  Typical usage example:
+
+  from pathlib import Path
+  from pyqmrlab.mt import MTsat
+
+  mt_sat_obj = MTsat()
+  
+  # Download sample dataset
+  mt_sat_obj.download(folder='data/')
+
+  # Load data
+  data_folder = Path('data/mt_sat/')
+  mt_sat_obj.load(
+      MTw = data_folder / 'MTw.nii.gz',
+      PDw = data_folder / 'PDw.nii.gz',
+      T1w = data_folder / 'T1w.nii.gz'
+      )
+
+  # Fit data
+  mt_sat_obj.fit()
+
+  # Save to NIFTI
+  mt_sat_obj.save(filenames = [
+      data_folder / 'MTsat.nii', 
+      data_folder / 'T1.nii'
+      ])
+"""
+
+from pyqmrlab.abstract import *
 
 np.seterr(divide="ignore", invalid="ignore")
 
 
-class mtsat(Abstract):
+class MTsat(Abstract):
+    """Magnetization Transfer saturation (MTsat) data processing class
+
+    Fits MTsat data and saves to NIfTI. Demo dataset available for download.
+
+    Attributes:
+        data_url: Link to demo dataset.
+        params: Dictionnary of pulse sequence parameters for each 
+                measurements. Measurement keys: MTw, T1w, PDw. Keys for each 
+                measurement: flip_angle (degrees) and repetition_time 
+                (seconds).
+    """
+
     data_url = "https://osf.io/c5wdb/download?version=3"
+    params = None
 
     def __init__(self, params=None):
-        pass
+        """Initializes a MTsat object.
 
+        Assigns MTsat pulse sequence parameters to that `params` attribute, or
+        if none are given assigns the parameters to be used with the demo data.
+
+        Args:
+            params: Dictinonnary of pulse sequence parameters for each
+                measurements. Measurement keys: MTw, T1w, PDw. Keys for each 
+                measurement: flip_angle (degrees) and repetition_time 
+                (seconds).
+
+        Returns:
+            MTsat class object with parameters initialized.
+        """
         if params == None:
             self.params = {
-                "MTw": {"FA": 6, "TR": 0.028},
-                "T1w": {"FA": 20, "TR": 0.018},
-                "PDw": {"FA": 6, "TR": 0.028},
+                "MTw": {"flip_angle": 6, "repetition_time": 0.028},
+                "T1w": {"flip_angle": 20, "repetition_time": 0.018},
+                "PDw": {"flip_angle": 6, "repetition_time": 0.028},
             }
 
     def load(self, MTw, PDw, T1w, Mask=None):
@@ -29,7 +80,6 @@ class mtsat(Abstract):
         super().load(args)
 
     def save(self, filenames=None):
-
         if filenames == None:
             filename = ["MTsat.nii.gz", "T1.nii.gz"]
 
@@ -37,15 +87,23 @@ class mtsat(Abstract):
         super().save(self.T1, filenames[1])
 
     def fit(self):
+        """Calculates MTsat and T1 maps from an MTsat dataset.
 
-        FA_MTw = self.params["MTw"]["FA"]
-        TR_MTw = self.params["MTw"]["TR"]
-        FA_PDw = self.params["PDw"]["FA"]
-        TR_PDw = self.params["PDw"]["TR"]
-        FA_T1w = self.params["T1w"]["FA"]
-        TR_T1w = self.params["T1w"]["TR"]
+        Referenes:
+            Helms, G., Dathe, H., Kallenberg, K., Dechent, P., 2008. 
+            High-resolution maps of magnetization transfer with inherent 
+            correction for RF inhomogeneity and T1 relaxation obtained from 3D
+            FLASH MRI. Magn. Reson. Med. 60, 1396-1407.
+        """
 
-        # Convert FA's from deg to rad
+        FA_MTw = self.params["MTw"]["flip_angle"]
+        TR_MTw = self.params["MTw"]["repetition_time"]
+        FA_PDw = self.params["PDw"]["flip_angle"]
+        TR_PDw = self.params["PDw"]["repetition_time"]
+        FA_T1w = self.params["T1w"]["flip_angle"]
+        TR_T1w = self.params["T1w"]["repetition_time"]
+
+        # Convert flip_angles from deg to rad
         FA_MTw = np.deg2rad(FA_MTw)
         FA_PDw = np.deg2rad(FA_PDw)
         FA_T1w = np.deg2rad(FA_T1w)
@@ -62,7 +120,6 @@ class mtsat(Abstract):
         self._apply_mask(MTsat=self.MTsat, T1=self.T1)
 
     def __R1(self, T1w, PDw, FA_T1w, TR_T1w, FA_PDw, TR_PDw):
-
         num_1 = (FA_T1w / TR_T1w) * T1w
         num_2 = (FA_PDw / TR_PDw) * PDw
         denum_1 = PDw / FA_PDw
@@ -70,7 +127,6 @@ class mtsat(Abstract):
         return 0.5 * (num_1 - num_2) / (denum_1 - denum_2)
 
     def __A(self, T1w, PDw, FA_T1w, TR_T1w, FA_PDw, TR_PDw):
-
         num_1 = TR_PDw * FA_T1w / FA_PDw
         num_2 = TR_T1w * FA_PDw / FA_T1w
         num_3 = PDw * T1w
